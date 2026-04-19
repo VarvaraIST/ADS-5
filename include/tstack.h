477d04745b6d2f1dd38a3e4a46405b1494a788dd
VarvaraSed <varvarasedyshevafgh@gmail.com>
2026-04-19 20:32:47 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,42 +2,39 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template<typename T, int N>
 class TStack {
  private:
-    T data[size];
+    T data[N];
     int top_index;
-
+    
  public:
-    TStack() : top_index(0) {
-    for (int i = 0; i < size; ++i) {
-        data[i] = T();     
-    }
-
+    TStack() : top_index(0), data{} {}
+    
     void push(const T& value) {
-        if (top_index < size) {
+        if (top_index < N) {
             data[top_index++] = value;
         }
     }
-
+    
     T pop() {
         if (top_index > 0) {
             return data[--top_index];
         }
         return T();
     }
-
+    
     T top() const {
         if (top_index > 0) {
             return data[top_index - 1];
         }
         return T();
     }
-
+    
     bool is_empty() const {
         return top_index == 0;
     }
-
+    
     int size() const {
         return top_index;
     }
