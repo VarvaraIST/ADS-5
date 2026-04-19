f705ab4f91c8b7b6fe05e23009bd369e9b95cc8e
VarvaraSed <varvarasedyshevafgh@gmail.com>
2026-04-19 19:53:00 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,43 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2026 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T data[N];
+    int top_index;
+
+public:
+    TStack() : top_index(0) {}
+
+    void push(const T& value) {
+        if (top_index < N) {
+            data[top_index++] = value;
+        }
+    }
+
+    T pop() {
+        if (top_index > 0) {
+            return data[--top_index];
+        }
+        return T();
+    }
+
+    T top() const {
+        if (top_index > 0) {
+            return data[top_index - 1];
+        }
+        return T();
+    }
+
+    bool is_empty() const {
+        return top_index == 0;
+    }
+
+    int size() const {
+        return top_index;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
