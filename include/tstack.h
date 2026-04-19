7f992f04b6d46dcbff00cd4705503861c9f4ea05
VarvaraSed <varvarasedyshevafgh@gmail.com>
2026-04-19 20:48:16 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -7,37 +7,31 @@ class TStack {
  private:
     T data[N];
     int top_index;
-    
  public:
     TStack() : top_index(0), data{} {}
-    
     void push(const T& value) {
         if (top_index < N) {
             data[top_index++] = value;
         }
     }
-    
     T pop() {
         if (top_index > 0) {
             return data[--top_index];
         }
         return T();
     }
-    
     T top() const {
         if (top_index > 0) {
             return data[top_index - 1];
         }
         return T();
     }
-    
     bool is_empty() const {
         return top_index == 0;
     }
-    
     int size() const {
         return top_index;
     }
 };
 
-#endif
+#endif //INCLUDE_TSTACK_H_
