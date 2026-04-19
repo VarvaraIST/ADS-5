b769b29fb935a9632931c8d97701628e156a37a1
VarvaraSed <varvarasedyshevafgh@gmail.com>
2026-04-19 20:29:00 +0300

    Update alg.h

--- include/alg.h
+++ include/alg.h
@@ -3,9 +3,8 @@
 #define INCLUDE_ALG_H_
 
 #include <string>
-#include "tstack.h"
 
-std::string infx2pstfx(const std::string& inf);
-int eval(const std::string& pref);
+std::string infx2pstfx(std::string& inf);
+int eval(std::string& pref);
 
 #endif
