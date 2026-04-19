11b6c8e50a1ff21627302aac001ec3eab3355a28
VarvaraSed <varvarasedyshevafgh@gmail.com>
2026-04-19 20:14:59 +0300

    Update alg.h

--- include/alg.h
+++ include/alg.h
@@ -1,11 +1,11 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2026 NNTU-CS
 #ifndef INCLUDE_ALG_H_
 #define INCLUDE_ALG_H_
 
 #include <string>
 #include "tstack.h"
 
-std::string infx2pstfx(std::string);
-int eval(std::string);
+std::string infx2pstfx(const std::string& inf);
+int eval(const std::string& pref);
 
 #endif
